4dc94979507f75f595b80f4e7e676174b21c9a61
Misasasasasaka <misasasasasaka@gmail.com>
2025-12-24 00:06:39 +0800

    添加三个app基础结构和models TODO

---
+++ books/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# TODO: 创建Category分类模型
+# TODO: 创建Book图书模型
+# TODO: 添加字段：title(书名), author(作者), isbn(唯一), publisher(出版社)
+# TODO: 添加字段：publish_date(出版日期), description(简介)
+# TODO: 添加字段：category(外键关联Category)
+# TODO: 添加字段：cover(封面图片)
+# TODO: 添加字段：total_copies(总数量), available_copies(可借数量)
+# TODO: 添加字段：location(书架位置)
+# TODO: 添加字段：status(上架/下架状态)
+# TODO: 添加字段：created_at, updated_at（时间戳）
---
+++ borrows/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.conf import settings
+
+
+# TODO: 创建Borrow借阅记录模型
+# TODO: 添加外键：user(关联用户), book(关联图书)
+# TODO: 添加字段：borrow_date(借阅时间), due_date(应还日期), return_date(实际归还时间)
+# TODO: 添加字段：status(borrowed/returned/overdue三种状态)
+# TODO: ...
